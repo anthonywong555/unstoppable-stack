8242abcf5bc6034c8f2dbf3e888f2a14f34390a6
anthonywong555 <anthonywong555@gmail.com>
2026-06-15 05:17:08 -0400

    Making the main area more scrollable. Showing the setting dialog

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -4,8 +4,8 @@
 	import { Toaster } from "$lib/components/ui/sonner/index.js";
 	import { ModeWatcher } from "mode-watcher";
 	import type { LayoutProps } from './$types';
-	import { getStatus } from '../routes/durable-execution/data.remote';
 	import AppSidebar from "$lib/components/layout/app-sidebar.svelte";
+	import Settings from "$lib/components/layout/nav-settings.svelte";
 	import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
 
@@ -22,23 +22,21 @@
 <Sidebar.Provider>
 	<AppSidebar />
 	<Sidebar.Inset>
-		<header class="flex items-center h-16 shrink-0 gap-2 px-4 justify-between">
-				<Sidebar.Trigger class="-ms-1" />
-				<Breadcrumb.Root>
-					<Breadcrumb.List>
-						<Breadcrumb.Item class="hidden md:block">
-							<Breadcrumb.Link href="##">Build Your Application</Breadcrumb.Link>
-						</Breadcrumb.Item>
-						<Breadcrumb.Separator class="hidden md:block" />
-						<Breadcrumb.Item>
-							<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
-						</Breadcrumb.Item>
-					</Breadcrumb.List>
-				</Breadcrumb.Root>
-				<Sidebar.Trigger class="-ms-1" />
-			</header>
-		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-			{@render children()}
-		</div>
+		<header class="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
+      <Sidebar.Trigger class="-ms-1" />
+      <Breadcrumb.Root>
+        <Breadcrumb.List>
+          <Breadcrumb.Item>
+            <Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
+          </Breadcrumb.Item>
+        </Breadcrumb.List>
+      </Breadcrumb.Root>
+			<Settings />
+    </header>
+		<div class="flex flex-1 flex-col gap-4 p-4">
+      {#each Array.from({ length: 24 }) as _, index (index)}
+        <div class="bg-muted/50 aspect-video h-12 w-full rounded-lg"></div>
+      {/each}
+    </div>
 	</Sidebar.Inset>
 </Sidebar.Provider>
