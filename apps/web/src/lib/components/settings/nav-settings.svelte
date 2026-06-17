84de74def8f6deb458d0b8dc45fb604161aef777
anthonywong555 <anthonywong555@gmail.com>
2026-06-17 05:27:27 -0400

    Adding a non-blocking logic for fetching services reachability

--- apps/web/src/lib/components/settings/nav-settings.svelte
+++ apps/web/src/lib/components/settings/nav-settings.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
   import Settings from "@lucide/svelte/icons/settings";
+  import { Spinner } from "$lib/components/ui/spinner/index.js";
   import { MediaQuery } from "svelte/reactivity";
   import * as Dialog from "$lib/components/ui/dialog/index.js";
   import * as Drawer from "$lib/components/ui/drawer/index.js";
   import { Button, buttonVariants } from "$lib/components/ui/button/index.js";
   import * as Table from "$lib/components/ui/table/index.js";
   import { Badge } from "$lib/components/ui/badge/index.js";
+  import { getStatus } from '../../../routes/durable-execution/data.remote';
 
+	const query = getStatus();
   
   let open = $state(false);
   const isDesktop = new MediaQuery("(min-width: 768px)");
 
   const id = $props.id();
 </script>
- 
+
 {#if isDesktop.current}
  <Dialog.Root bind:open>
   <Dialog.Trigger class={buttonVariants({ variant: 'ghost', size: 'icon-sm'})}>
@@ -38,7 +41,15 @@
       <Table.Row>
         <Table.Cell class="font-medium">Durable Execution</Table.Cell>
         <Table.Cell>
-          <Badge variant="secondary" class="bg-green-500">Online</Badge>
+          {#if query.error}
+            <Badge variant="destructive">Offline</Badge>
+          {:else if query.loading}
+            <Badge><Spinner />Loading</Badge>
+          {:else if query.current == true}
+            <Badge variant="secondary" class="bg-green-500">Online</Badge>
+          {:else}
+            <Badge variant="destructive">Offline</Badge>
+          {/if}
         </Table.Cell>
         <Table.Cell>
           <Button variant="outline">Dashboard</Button>
